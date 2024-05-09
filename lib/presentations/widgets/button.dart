ccd869050cf5d0d2fceb42066c8778521a099db5
Hilaladiii <abdulhilal242@gmail.com>
2024-05-09 21:10:58 +0700

    feat : add custom widget button

--- lib/presentations/widgets/button.dart
+++ lib/presentations/widgets/button.dart
@@ -1,18 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:serenify/styles/my_text.dart';
 
+enum Size { big, small }
+
 class Button extends StatelessWidget {
   const Button(
       {super.key,
       required this.label,
       required this.color,
       required this.onPressed,
-      required this.backgroundColor});
+      required this.backgroundColor,
+      required this.size});
 
   final String label;
   final Color color;
   final Color backgroundColor;
   final VoidCallback onPressed;
+  final Size size;
 
   @override
   Widget build(BuildContext context) {
@@ -21,13 +25,18 @@ class Button extends StatelessWidget {
         style: ElevatedButton.styleFrom(
             backgroundColor: backgroundColor,
             elevation: 0,
-            visualDensity: const VisualDensity(
-                horizontal: VisualDensity.maximumDensity, vertical: 1),
+            visualDensity: size == Size.big
+                ? const VisualDensity(
+                    horizontal: VisualDensity.maximumDensity, vertical: 1)
+                : const VisualDensity(
+                    horizontal: 0, vertical: VisualDensity.minimumDensity),
             shape: RoundedRectangleBorder(
                 borderRadius: BorderRadius.circular(25))),
         child: Text(
           label,
-          style: MyTextStyle.h6semi(color: color),
+          style: size == Size.big
+              ? MyTextStyle.h6semi(color: color)
+              : MyTextStyle.h8semi(color: color),
         ));
   }
 }
