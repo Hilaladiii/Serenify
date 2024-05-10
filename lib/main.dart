209c0db58cbd03202678ab8c34f68cc647a598b0
Hilaladiii <abdulhilal242@gmail.com>
2024-05-10 21:28:28 +0700

    feat : configure bloc provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:serenify/bloc/article/article_bloc.dart';
 import 'package:serenify/bloc/auth/auth_bloc.dart';
+import 'package:serenify/bloc/community/community_bloc.dart';
+import 'package:serenify/bloc/seminar/seminar_bloc.dart';
+import 'package:serenify/bloc/test/test_bloc.dart';
+import 'package:serenify/bloc/user/user_bloc.dart';
 import 'package:serenify/presentations/pages/splash/splash_page.dart';
 
 void main() {
@@ -15,6 +20,11 @@ class MyApp extends StatelessWidget {
     return MultiBlocProvider(
       providers: [
         BlocProvider(create: (context) => AuthBloc()),
+        BlocProvider(create: (context) => TestBloc()),
+        BlocProvider(create: (context) => CommunityBloc()),
+        BlocProvider(create: (context) => ArticleBloc()),
+        BlocProvider(create: (context) => SeminarBloc()),
+        BlocProvider(create: (context) => UserBloc()),
       ],
       child: MaterialApp(
         title: 'Serenify',
