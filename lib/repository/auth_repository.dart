92625516ec9e5d1999d22b2743ccb85a72c0a32d
Hilaladiii <abdulhilal242@gmail.com>
2024-05-10 21:25:14 +0700

    feat : add auth repository

--- lib/repository/auth_repository.dart
+++ lib/repository/auth_repository.dart
@@ -1,5 +1,6 @@
 import 'package:dio/dio.dart';
 import 'package:serenify/models/status_model.dart';
+import 'package:serenify/models/user_model.dart';
 import 'package:serenify/utils/api.dart';
 
 class AuthRepository {
@@ -33,15 +34,34 @@ class AuthRepository {
         "password": password,
       });
 
-      if (response.statusCode == 401) {
-        return StatusModel.fromJson(response.data, null);
-      } else if (response.statusCode == 200) {
+      if (response.statusCode == 200) {
         return StatusModel.fromJson(response.data, null);
       } else {
         return StatusModel.fromJson(response.data, null);
       }
     } on DioException catch (e) {
       return StatusModel.fromJson(e.response!.data, null);
+    } catch (e) {
+      throw Exception(e.toString());
+    }
+  }
+
+  Future<UserModel> getUser({required String token}) async {
+    try {
+      var response = await dio.get(ApiInstance().pathUrl('/auth/current-user'),
+          options: Options(headers: {"Authorization": "Bearer $token"}));
+      return UserModel.fromJson(response.data);
+    } catch (e) {
+      throw Exception(e.toString());
+    }
+  }
+
+  Future<UserScoreModel> getUserScore({required String token}) async {
+    try {
+      var response = await dio.get(ApiInstance().pathUrl('/user/score'),
+          options: Options(headers: {"Authorization": "Bearer $token"}));
+      print(response.data);
+      return UserScoreModel.fromJson(response.data);
     } catch (e) {
       throw Exception(e.toString());
     }
