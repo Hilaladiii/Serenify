da191422807cb9b13d030251c5560cc0f885dcca
Hilaladiii <abdulhilal242@gmail.com>
2024-05-10 21:27:57 +0700

    feat : add user model

--- lib/models/user_model.dart
+++ lib/models/user_model.dart
@@ -1,6 +1,34 @@
 class UserModel {
-  final String username;
+  final String name;
   final String email;
+  final String birthData;
+  final String photoLink;
 
-  UserModel({required this.username, required this.email});
+  UserModel(
+      {required this.name,
+      required this.email,
+      required this.birthData,
+      required this.photoLink});
+
+  factory UserModel.fromJson(Map<String, dynamic> json) {
+    var data = json['data'];
+    return UserModel(
+        name: data['name'],
+        email: data['email'],
+        birthData: data['birth_date'],
+        photoLink: data['photo_link']);
+  }
+}
+
+class UserScoreModel {
+  final int score;
+  final String description;
+
+  UserScoreModel({required this.score, required this.description});
+
+  factory UserScoreModel.fromJson(Map<String, dynamic> json) {
+    var data = json['data'];
+    return UserScoreModel(
+        score: data['score'], description: data['description']);
+  }
 }
