ce3c86fbc650d23a8b5b362fa26dde4040618482
Hilaladiii <abdulhilal242@gmail.com>
2024-05-10 23:11:26 +0700

    feat : add seminar repository

---
+++ lib/repository/seminar_repository.dart
@@ -0,0 +1,26 @@
+import 'package:dio/dio.dart';
+import 'package:serenify/models/seminar_model.dart';
+import 'package:serenify/utils/api.dart';
+
+class SeminarRepository {
+  Dio dio = Dio();
+
+  Future<SeminarData> getAllSeminar() async {
+    try {
+      var response = await dio.get(ApiInstance().pathUrl('/seminar'));
+      return SeminarData.fromJson(response.data);
+    } catch (e) {
+      throw Exception(e.toString());
+    }
+  }
+
+  Future<DetailSeminarModel> getSeminarById({required String id}) async {
+    try {
+      var response = await dio.get(ApiInstance().pathUrl('/seminar/$id'));
+      print(response.data);
+      return DetailSeminarModel.fromJson(response.data);
+    } catch (e) {
+      throw Exception(e.toString());
+    }
+  }
+}
