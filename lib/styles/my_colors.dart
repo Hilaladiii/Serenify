87a9eab206d45571798bbaa4b168c93e4213d50a
Hilaladiii <abdulhilal242@gmail.com>
2024-05-10 21:23:08 +0700

    feat : add custom colors

--- lib/styles/my_colors.dart
+++ lib/styles/my_colors.dart
@@ -1,10 +1,12 @@
 import 'dart:ui';
 
-class MyColors{
+class MyColors {
   static Color gray50 = const Color(0xFFFAFAFA);
   static Color gray100 = const Color(0xFFF1F1F1);
   static Color gray200 = const Color(0xFFD5D5D8);
   static Color gray300 = const Color(0xFFAEAEB3);
   static Color gray400 = const Color(0xFF88888F);
   static Color tertiary400 = const Color(0xFF3B8ACF);
+  static Color primaryBase = const Color(0xFFCD1922);
+  static Color quaternary200 = const Color(0xFF00D32C);
 }
